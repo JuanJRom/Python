1fda71b0b593d321440945436cb789eb87e63f25
Juan Romero <jjose.romero@alumnos.udg.mx>
2020-06-26 16:15:35 -0500

    Tarea 4

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+
+x = 0
+if x:
+    string='x no es cero'
+else:
+    string='x no es cero'
+print(string)
+
+y = 3
+
+if x > y:
+    print('x es mayor')
+else:
+    print('y es mayor')
+
---
+++ listas.py
@@ -0,0 +1,7 @@
+l = [1, 2.7, True, 'Hola']
+
+for element in l:
+    print(element)
+
+for index, element in enumerate(l):
+    print(index, ':', element)
